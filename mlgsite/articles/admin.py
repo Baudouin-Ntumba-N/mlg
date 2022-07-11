852cfa63dd87932d869b4e90020542ba244a4881
Baudouin-Ntumba-N <bntumbanyembwe@gmail.com>
2022-07-11 19:06:25 +0200

    mlgsite taken away

--- mlgsite/articles/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from articles.models import Article, Comment, CommentAdmin, Reponse, ReponseAdmin
-admin.site.register(Article)
-admin.site.register(Comment, CommentAdmin)
-admin.site.register(Reponse, ReponseAdmin)
